05c7fc810b35a44b51fb315e68f341f6bd8cc0a8
Daniel Schepler <dschepler@gmail.com>
2015-04-19 08:14:06 -0700

    Convert Homeomorphisms to type classes

--- Homeomorphisms.v
+++ Homeomorphisms.v
@@ -1,88 +1,83 @@
 Require Export TopologicalSpaces.
 Require Export Continuity.
+Require Import EnsemblesUtf8.
+Require Import FunctionalExtensionality.
 
-Inductive homeomorphism {X Y:TopologicalSpace}
-  (f:point_set X -> point_set Y) : Prop :=
-| intro_homeomorphism: forall g:point_set Y -> point_set X,
-  continuous f -> continuous g ->
-  (forall x:point_set X, g (f x) = x) ->
-  (forall y:point_set Y, f (g y) = y) -> homeomorphism f.
+Generalizable All Variables.
 
-Lemma homeomorphism_is_invertible: forall {X Y:TopologicalSpace}
-  (f:point_set X -> point_set Y),
-  homeomorphism f -> invertible f.
+Inductive homeomorphism `{OpenSets X} `{OpenSets Y} (f : X → Y) : Prop :=
+| intro_homeomorphism : ∀ g : Y → X,
+  continuous f → continuous g → f ○ g = id → g ○ f = id →
+  homeomorphism f.
+
+Lemma homeomorphism_is_invertible : ∀ `{OpenSets X} `{OpenSets Y}
+  (f : X → Y), homeomorphism f → invertible f.
 Proof.
-intros.
-destruct H as [g].
-exists g; trivial.
+intros. destruct H1 as [g]. exists g; trivial.
++ exact (equal_f H4).
++ exact (equal_f H3).
 Qed.
 
-Definition open_map {X Y:TopologicalSpace}
-  (f:point_set X -> point_set Y) : Prop :=
-forall U:Ensemble (point_set X), open U -> open (Im U f).
-
-Lemma homeomorphism_is_open_map: forall {X Y:TopologicalSpace}
-  (f:point_set X -> point_set Y),
-  homeomorphism f -> open_map f.
+Lemma homeomorphism_id : ∀ `{OpenSets X}, homeomorphism (@id X).
 Proof.
-intros.
-destruct H as [g].
-red; intros.
-assert (Im U f = inverse_image g U).
+intros. exists id; auto using continuous_identity.
+Qed.
+
+Lemma homeomorphism_composition : ∀ `{OpenSets X} `{OpenSets Y} `{OpenSets Z}
+  (f : Y → Z) (g : X → Y), homeomorphism f → homeomorphism g →
+  homeomorphism (f ○ g).
+Proof.
+intros. destruct H2 as [finv], H3 as [ginv].
+exists (ginv ○ finv); eauto using @continuous_composition.
++ change (f ○ (g ○ ginv) ○ finv = id). rewrite H8. assumption.
++ change (ginv ○ (finv ○ f) ○ g = id). rewrite H6. assumption.
+Qed.
+
+Definition open_map `{OpenSets X} `{OpenSets Y} (f : X → Y) : Prop :=
+∀ U:Ensemble X, open U → open (Im U f).
+
+Lemma homeomorphism_is_open_map : ∀ `{OpenSets X} `{OpenSets Y} (f : X → Y),
+  homeomorphism f → open_map f.
+Proof.
+intros. destruct H1 as [g]. red; intros.
+replace (Im U f) with (inverse_image g U); auto.
 apply Extensionality_Ensembles; split; red; intros.
-destruct H4.
-rewrite H5.
-constructor.
-rewrite H1; trivial.
-destruct H4.
-exists (g x); trivial.
-symmetry; apply H2.
-rewrite H4; apply H0; trivial.
++ destruct H6. exists (g x); trivial. symmetry. exact (equal_f H3 x).
++ destruct H6 as [y]. subst y0. constructor. pose proof (equal_f H4 y).
+  unfold composition in H7. unfold id in H7. rewrite H7. assumption.
 Qed.
 
-Lemma invertible_open_map_is_homeomorphism: forall {X Y:TopologicalSpace}
-  (f:point_set X -> point_set Y),
-  invertible f -> continuous f -> open_map f -> homeomorphism f.
+Lemma invertible_open_map_is_homeomorphism :
+  ∀ `{OpenSets X} `{OpenSets Y} (f : X → Y), invertible f → continuous f →
+  open_map f → homeomorphism f.
 Proof.
-intros.
-destruct H as [g].
-exists g; trivial.
-red; intros.
-assert (inverse_image g V = Im V f).
-apply Extensionality_Ensembles; split; red; intros.
-destruct H4.
-exists (g x); trivial.
-symmetry; apply H2.
-destruct H4.
-constructor.
-rewrite H5.
-rewrite H; trivial.
-rewrite H4; apply H1; trivial.
+intros. destruct H1 as [g]. exists g; trivial.
++ red; intros. replace (inverse_image g V) with (Im V f); auto.
+  apply Extensionality_Ensembles; split; red; intros.
+  - destruct H6 as [y]. subst y0. constructor. rewrite H1. assumption.
+  - destruct H6. exists (g x); trivial. symmetry; trivial.
++ extensionality y; apply H4.
++ extensionality x; apply H1.
 Qed.
 
-Inductive homeomorphic (X Y:TopologicalSpace) : Prop :=
-| intro_homeomorphic: forall f:point_set X -> point_set Y,
-    homeomorphism f -> homeomorphic X Y.
+Inductive homeomorphic (X:Type) `{OpenSets X} (Y:Type) `{OpenSets Y} : Prop :=
+| intro_homeomorphic : ∀ f : X → Y, homeomorphism f → homeomorphic X Y.
+Existing Class homeomorphic.
 
-Require Export Relation_Definitions.
-Require Import Relation_Definitions_Implicit.
-
-Lemma homeomorphic_equiv: equivalence homeomorphic.
+Lemma homeomorphic_refl : ∀ `{OpenSets X}, homeomorphic X X.
 Proof.
-constructor.
-red; intros X.
-exists (fun x:point_set X => x).
-exists (fun x:point_set X => x); trivial;
-  apply continuous_identity.
+intros. exists id. auto using @homeomorphism_id.
+Qed.
 
-red; intros X Y Z ? ?.
-destruct H as [f [finv]].
-destruct H0 as [g [ginv]].
-exists (fun x:point_set X => g (f x)).
-exists (fun z:point_set Z => finv (ginv z)); (congruence ||
-  apply continuous_composition; trivial).
+Lemma homeomorphic_sym : ∀ `{OpenSets X} `{OpenSets Y},
+  homeomorphic X Y → homeomorphic Y X.
+Proof.
+intros. destruct H1 as [f [g] ]. exists g; exists f; trivial.
+Qed.
 
-red; intros X Y ?.
-destruct H as [f [finv]].
-exists finv; exists f; trivial.
+Lemma homeomorphic_trans : ∀ `{OpenSets X} `{OpenSets Y} `{OpenSets Z},
+  homeomorphic X Y → homeomorphic Y Z → homeomorphic X Z.
+Proof.
+intros. destruct H2 as [g], H3 as [f]. exists (f ○ g).
+eauto using @homeomorphism_composition.
 Qed.
